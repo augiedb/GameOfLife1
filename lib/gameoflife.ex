5b7b1d0afafb3d105776c24ba84ec11ec30057ae
Augie De Blieck Jr <augiedb@gmail.com>
2015-09-27 20:57:48 +0000

    Cells change correctly now
    Still adding tests, but it looks like the code will handle changing a live cell to dead and vice versa correctly.
    
    Huzzah!

--- lib/gameoflife.ex
+++ lib/gameoflife.ex
@@ -11,7 +11,7 @@ defmodule Gameoflife do
   end
 
   defp create_empty_board do
-    for x <- 1..@size, y <- 1..@size, 
+    for x <- 0..(@size+1), y <- 0..(@size+1), 
         do: {{x,y}, "."}, 
       into: HashDict.new
   end
@@ -19,7 +19,7 @@ defmodule Gameoflife do
   defp add_live_cells(board) do
     total = @size * @size
     live_squares_wanted = round( (@live_start / 100) * total)
-    add_live_square(board, live_squares_wanted, 1)
+    add_live_square(board, live_squares_wanted + 2, 1)
   end
 
   def add_live_square(board, portion, portion) do
@@ -27,9 +27,9 @@ defmodule Gameoflife do
   end
 
   def add_live_square(board, portion, acc) do
+    :random.seed(:os.timestamp)
     x = :random.uniform(@size)    
     y = :random.uniform(@size)    
-    # NEXT TO DO: is_cell_alive();#******
     case HashDict.get(board, {x,y}) do
       "*" -> add_live_square(board, portion, acc)
       "." -> board = HashDict.put(board, {x,y}, "*")   
@@ -60,15 +60,29 @@ defmodule Gameoflife do
   end
 
   def run_through_cells(board) do
-    new_board = Gameoflife.init
-    for x <- 1..@size, y <- 1..@size do
-      new_cell  = update_cell(board, {x,y})
-      new_board = HashDict.put(new_board, {x,y}, new_cell )
-    end 
-    new_board
-   end
+    do_run_through_cells(board, 1, 1, Gameoflife.init)
+  end
 
-  def update_cell(board, {x, y}) do
+  # You've fallen off the bottom of the board
+  def do_run_through_cells(_, @size+1, _, new_board) do
+    new_board
+  end 
+
+  # Next row after this one
+  def do_run_through_cells(board, x, y, new_board) when y == @size do
+    new = new_cell_value(board, {x,y})
+    new_board = HashDict.put(new_board, {x, y}, new)
+    do_run_through_cells(board, x+1, 1, new_board)
+  end
+
+  # Next!
+  def do_run_through_cells(board, x, y, new_board) do
+    new = new_cell_value(board, {x,y})
+    new_board = HashDict.put(new_board, {x, y}, new)
+    do_run_through_cells(board, x, y+1, new_board)
+  end
+
+  def new_cell_value(board, {x, y}) do
      case(HashDict.get(board, {x,y}) ) do
         "*" -> check_live_cell(board, {x, y})
         "." -> check_dead_cell(board, {x, y})
@@ -76,24 +90,23 @@ defmodule Gameoflife do
   end
 
   def check_dead_cell(board, {x, y}) do
-    
     live = live_cells_surrounding(board, {x, y})
     case live do
       3 -> "*"
       _ -> "."
     end
-
   end
 
   def check_live_cell(board, {x, y}) do
-
     live = live_cells_surrounding(board, {x,y})
+IO.puts "LIVE #{live}"
     case live do
-      0..1 -> "." #Dies
-      2..3 -> "*" #Lives
-      4..8 -> "." #Dies 
+      0 -> "."
+      1 -> "."
+      2 -> "*"
+      3 -> "*"
+      _ -> "."
     end
-  
   end
 
 
--- test/gameoflife_test.exs
+++ test/gameoflife_test.exs
@@ -8,13 +8,41 @@ defmodule GameoflifeTest do
 
   test "set up board" do
     board = Gameoflife.init
-    assert HashDict.size(board) == 100
+    assert HashDict.size(board) == 144
   end
 
   test "Board prints out correctly" do
-    board = Gameoflife.init
+   # board = Gameoflife.init
 #    assert Gameoflife.get_row(board, 1) == "0000000000" 
 #    assert Gameoflife.display_board(board) = "notgonnawork"
   end
+
+  test "Test cell changes" do
+    sample_cell = Gameoflife.init
+    sample_cell = HashDict.put(sample_cell, {0, 0 }, ".")
+    sample_cell = HashDict.put(sample_cell, {0, 1 }, ".")
+    sample_cell = HashDict.put(sample_cell, {0, 2 }, ".")
+
+    sample_cell = HashDict.put(sample_cell, {1, 0 }, ".")
+    sample_cell = HashDict.put(sample_cell, {1, 1 }, ".")
+    sample_cell = HashDict.put(sample_cell, {1, 2 }, ".")
+
+    sample_cell = HashDict.put(sample_cell, {2, 0 }, ".")
+    sample_cell = HashDict.put(sample_cell, {2, 1 }, ".")
+    sample_cell = HashDict.put(sample_cell, {2, 2 }, ".")
+
+    assert Gameoflife.new_cell_value(sample_cell, {1,1}) == "."
+    sample_cell = HashDict.put(sample_cell, {1, 1 }, "*")
+    assert Gameoflife.new_cell_value(sample_cell, {1,1}) == "."
+
+    sample_cell = HashDict.put(sample_cell, {1, 0 }, "*")
+    sample_cell = HashDict.put(sample_cell, {1, 1 }, ".")
+    sample_cell = HashDict.put(sample_cell, {1, 2 }, "*")
+    sample_cell = HashDict.put(sample_cell, {2, 1 }, "*")
+
+    assert Gameoflife.new_cell_value(sample_cell, {1,1}) == "*"
+    
+  end
+
    
 end
